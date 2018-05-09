6c03f989d749ba63aa5aff349619b7487006baeb
kejie <reach950@gmail.com>
2018-05-09 10:57:27 +0800

    优化创建删除笔记case

--- testcase/note/create_delete_note_test.py
+++ testcase/note/create_delete_note_test.py
@@ -29,9 +29,9 @@ class TestCreateDeleteNote(unittest.TestCase):
         self.note_page.tap_complete_button()
         self.note_page.tap_return_button()
         # 检查新创建的笔记是否在最新列表中显示
-        self.assertEqual(self.recent_page.get_first_note_title(), self.title, '笔记创建失败')
+        self.assertIsNotNone(self.recent_page.get_first_file_title(self.title), '笔记创建失败')
         self.recent_page.delete_first_file()
-        self.assertNotEqual(self.recent_page.get_first_note_title(), self.title, '笔记删除失败')
+        self.assertIsNone(self.recent_page.get_first_file_title(self.title), '笔记删除失败')
 
 
 if __name__ == '__main__':
