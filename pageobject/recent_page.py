c1b8d55c7cffa3908044dafd045d380c6168f6ea
kejie <reach950@gmail.com>
2018-05-09 10:57:05 +0800

    优化最新页面对象

--- pageobject/recent_page.py
+++ pageobject/recent_page.py
@@ -12,13 +12,10 @@ from pageobject.page_common_module import TabBar, CreateButtonsLayer
 
 class RecentPage(BasePage):
 
-    # 新创建笔记标题
-    first_note_title_loc = (MobileBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeCell/XCUIElementTypeStaticText[2]')
-
     # 第一个文件
     first_file = (MobileBy.CLASS_NAME, 'XCUIElementTypeCell')
 
-    # 删除按钮
+    # 第一个文件的删除按钮
     delete_button = (MobileBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeCell/XCUIElementTypeButton[3]')
 
     # 打开创建笔记页面
@@ -26,9 +23,11 @@ class RecentPage(BasePage):
         self.tap_element(TabBar.create_button_loc)
         self.tap_element(CreateButtonsLayer.create_note_buuton_loc)
 
-    # 获取新创建笔记的标题
-    def get_first_note_title(self):
-        return self.find_element(self.first_note_title_loc).get_attribute('value')
+    # 根据文本内容获取第一个文件的标题
+    def get_first_file_title(self, text):
+        first_file_title_loc = (MobileBy.IOS_CLASS_CHAIN, u'**/XCUIElementTypeCell/XCUIElementTypeStaticText'
+                                                          u'[`name == {}`]'.format(text))
+        return self.find_element(first_file_title_loc)
 
     # 删除第一个文件
     def delete_first_file(self):
